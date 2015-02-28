089d2983fd7de8a9d3ab5a8278d1a84f5cea1083
David DeGraw <david.degraw@gmail.com>
2015-02-28 11:10:18 -0700

    Update to contest code.

--- rocketLauncher/rocketLauncher.ino
+++ rocketLauncher/rocketLauncher.ino
@@ -40,13 +40,22 @@ void shoot() {
 }
 
 float pingBack() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2000);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(15);
-  digitalWrite(trigPin, LOW);
-  
-  pingTime = pulseIn(echoPin, HIGH); //in microseconds
+  float avg_array[3];
+  int i = 0;
+  float total = 0;
+  for(i=0; i<3; i += 1){
+     digitalWrite(trigPin, LOW);
+    delayMicroseconds(2000);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(15);
+    digitalWrite(trigPin, LOW);
+    avg_array[i] = pulseIn(echoPin, HIGH, 100000); //in microseconds
+    total += avg_array[i];
+    delay(10);
+  }
+  pingTime = total / 3;
+  //  pingTime = pulseIn(echoPin, HIGH,10000);
+ 
   pingTime = pingTime/1000000.0; //convert ping time to seconds
   pingTime = pingTime/3600.0; //convert ping time to hours
   
@@ -77,12 +86,12 @@ void sweep_y(){
 
 void sweep_x(){
   int pos = 0;
-  for(pos = 0; pos < 180; pos += 1){
-    float temp  [1]
+  for(pos = 0; pos < 180; pos += 10){
+    float temp  [1];
     x.write(pos);
     Serial.print("x is : ");
     Serial.println(pos);
-    delay(10);
+    delay(300);
     x_cords[pos] = pingBack();
     delay(10);
   }
@@ -91,9 +100,43 @@ void sweep_x(){
 void loop() {
   
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-
-  pingBack();
-  delay(100);
+  sweep_x();
+  y.write(90);
+  
+  
+  int i = 0;
+  float min_distance = 150;
+  int min_pos = 180;
+  for(i=0;i<180; i+=1){
+    if(x_cords[i] >= 10 && x_cords[i]<min_distance && i >= 35 ){
+      min_distance = x_cords[i];
+      min_pos = i;
+    }
+  }
+  x.write(min_distance);
+  Serial.print(min_pos);
+  
+  i = 0;
+  for(i = 0; i<100; i +=1){
+    x.write(min_pos);
+    delay(10);
+    
+  }
+  
+  shoot();
+  y.write(80); 
+  delay(250);
+ 
+  shoot();
+  y.write(60);
+  delay(250);
+  
+  shoot();
+  delay(250);
+  y.write(90);
+  delay(150);
+  
+  
 
 //    shoot();
 
